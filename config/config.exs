b4ddb890ad98a6320d287c788322b8216be7bda8
madclaws <pavanan.anandu63@gmail.com>
2021-07-10 20:02:43 +0530

    feat: added Horde and libcluster
    Replaced the supervisor and registry with Horde's. Also added libcluster

---
+++ config/config.exs
@@ -0,0 +1,4 @@
+use Mix.Config
+
+config :immortals,
+  nodes: ["alice@127.0.0.1", "bob@127.0.0.1"]
--- lib/immortals/application.ex
+++ lib/immortals/application.ex
@@ -12,13 +12,22 @@ defmodule Immortals.Application do
     children = [
       # Starts a worker by calling: Immortals.Worker.start_link(arg)
       # {Immortals.Worker, arg}
-      {Registry, keys: :unique, name: GodRegistry},
-      {DynamicSupervisor, name: GodSupervisor, strategy: :one_for_one}
+      {Cluster.Supervisor, [topologies(), name: Immortals.LibclusterSupervisor]},
+      {Horde.Registry, keys: :unique, name: GodRegistry},
+      {Horde.DynamicSupervisor, name: GodSupervisor, strategy: :one_for_one}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Immortals.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  defp topologies do
+    [
+      immortals: [
+        strategy: Cluster.Strategy.Gossip
+      ]
+    ]
   end
 end
